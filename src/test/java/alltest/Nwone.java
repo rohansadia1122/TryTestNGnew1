5aef00cacea2b45b3f9c8b7708941bfe1c11c0e6
rohansadia1122 <rohansadia1973@gmail.com>
2024-02-02 04:57:44 -0500

    commit
    done

---
+++ src/test/java/alltest/Nwone.java
@@ -0,0 +1,24 @@
+package alltest;
+
+public class Nwone {
+
+	public static void main(String[] args) {
+		
+		int counter;
+		
+		for (counter = 0; counter < 10; counter++)
+		    System.out.println(counter);
+
+		if (counter == 4) {
+
+		    continue;
+		}
+		
+		System.out.println("This will not get printed when counter is 4");
+		}
+
+
+
+	}
+
+
